f1b9cab0b868ce43aae27d0c024ff563b51dc5b0
M. Woellhaf <woellhaf@tu-berlin.de>
2017-11-23 17:04:27 +0100

    Updated elements.

--- elements.py
+++ elements.py
@@ -1,26 +1,31 @@
 import tensorflow as tf
 
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial, name='weight_variable')
+def weight_variable(shape, name='weight_variable'):
+    initial = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
+    return tf.get_variable(name=name, shape=shape, initializer=initial)
 
 
-def bias_variable(shape):
+def bias_variable(shape, name='bias_variable'):
     initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial, name='bias_variable')
+    return tf.get_variable(name=name, initializer=initial)
 
 
-def fc_layer(input_tensor, dim_in, dim_out, name='fc_layer', act=tf.nn.relu):
+def fc_layer(input_tensor, dim_in, dim_out, act=tf.nn.relu, dropout=None, name='fc_layer'):
     with tf.name_scope(name):
         W = weight_variable([dim_in, dim_out])
         b = bias_variable([dim_out])
-        return act(tf.matmul(input_tensor, W) + b)
+        y = act(tf.matmul(input_tensor, W) + b)
+        return tf.nn.dropout(y, dropout) if dropout else y
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d_layer(input_tensor, fshape, nchannels, nfilters, act=tf.nn.relu, name='conv2d_layer'):
+    with tf.name_scope(name):
+        W = weight_variable([fshape[0], fshape[1], nchannels, nfilters])
+        b = bias_variable([nfilters])
+        return act(tf.nn.conv2d(input_tensor, W, strides=[1, 1, 1, 1], padding='SAME') + b)
 
 
-def max_pool(x, ps=2, ss=2):
-    return tf.nn.max_pool(x, ksize=[1, ps, ps, 1], strides=[1, ss, ss, 1], padding='SAME')
+def max_pool_layer(input_tensor, ps=2, ss=2, name='max_pool_layer'):
+    with tf.name_scope(name):
+        return tf.nn.max_pool(input_tensor, ksize=[1, ps, ps, 1], strides=[1, ss, ss, 1], padding='SAME')
