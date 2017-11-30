96a960a9737f10c8a6c2a50d7f0eafe2a5ce789c
M. Woellhaf <woellhaf@tu-berlin.de>
2017-11-30 10:07:51 +0100

    Adjusted variable scopes of aes for more beautiful graphs.

--- util/elements.py
+++ util/elements.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
 
-def weight_variable(shape, name='weight_variable'):
+def weight_variable(shape=None, name='weight_variable'):
     initial = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
     return tf.get_variable(name=name, shape=shape, initializer=initial)
 
 
-def bias_variable(shape, name='bias_variable'):
-    initial = tf.constant(0.1, shape=shape)
+def bias_variable(shape=None, name='bias_variable'):
+    initial = tf.constant(0.1, shape=shape) if shape is not None else None
     return tf.get_variable(name=name, initializer=initial)
 
 
@@ -16,6 +16,15 @@ def fc_layer(input_tensor, dim_in, dim_out, act=tf.nn.relu, dropout=None, name='
         W = weight_variable([dim_in, dim_out])
         b = bias_variable([dim_out])
         y = act(tf.matmul(input_tensor, W) + b)
+        return tf.nn.dropout(y, dropout) if dropout is not None else y
+
+
+def fc_decode_layer(input_tensor, act=tf.nn.relu, dropout=None, name='fc_decode_layer'):
+    """ Layer shares weights with corresponding fully connected layer in the same variable scope."""
+    with tf.name_scope(name):
+        W = weight_variable()
+        b = bias_variable([W.get_shape()[0]], name='decode_bias')
+        y = act(tf.matmul(input_tensor, tf.transpose(W)) + b)
         return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
