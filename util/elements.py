8db325a3362e8ad78dc9fb2d55a4718d3a71a4dc
M. Wöllhaf <woellhaf@tu-berlin.de>
2017-12-19 18:07:52 +0100

    Extended conv2d_layer.

--- util/elements.py
+++ util/elements.py
@@ -28,11 +28,14 @@ def fc_decode_layer(input_tensor, act=tf.nn.relu, dropout=None, name='fc_decode_
         return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
-def conv2d_layer(input_tensor, fshape, nchannels, nfilters, act=tf.nn.relu, padding='SAME', name='conv2d_layer'):
+def conv2d_layer(input_tensor, fshape, nchannels, nfilters,
+                 strides=[1, 1, 1, 1], act=tf.nn.relu, dropout=None, padding='SAME', name='conv2d_layer'):
+
     with tf.name_scope(name):
         W = weight_variable([fshape[0], fshape[1], nchannels, nfilters])
         b = bias_variable([nfilters])
-        return act(tf.nn.conv2d(input_tensor, W, strides=[1, 1, 1, 1], padding=padding) + b)
+        y = act(tf.nn.conv2d(input_tensor, W, strides=strides, padding=padding) + b)
+        return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
 def max_pool_layer(input_tensor, ps=2, ss=2, name='max_pool_layer'):
