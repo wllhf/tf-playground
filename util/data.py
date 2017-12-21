aab43b6de1ddd15d20c76ea727bb58e61ebd09f2
M. Wöllhaf <woellhaf@tu-berlin.de>
2017-12-21 10:57:52 +0100

    Changed default preprocessing of standard data sets using Keras ImageDataGenerator.

--- util/data.py
+++ util/data.py
@@ -1,9 +1,8 @@
 import pickle
 
 import numpy as np
-
+from keras.preprocessing.image import ImageDataGenerator
 from img_toolbox import label
-from img_toolbox import adjustment
 from img_toolbox.data.mnist import mnist
 from img_toolbox.data.cifar import cifar10
 
@@ -14,52 +13,65 @@ def shuffle_data(x, y):
     return x[indices], y[indices]
 
 
-def load_mnist(path, flatten=True, standardize=True, shuffle=True):
-    # load data
+def preprocessing(x_train, x_test, per_image_standardize=True, whitening=True):
+
+    processor = ImageDataGenerator(
+        samplewise_std_normalization=per_image_standardize,
+        zca_whitening=whitening
+    )
+
+    processor.fit(x_train)
+    x_train = processor.standardize(x_train)
+    x_test = processor.standardize(x_test)
+
+    return (x_train, x_test)
+
+
+def load_mnist(path, flatten=False, standardize=True, whitening=True, shuffle=True, one_hot=True):
     mnist_obj = mnist(path)
-    trn = mnist_obj.train(flatten)
-    tst = mnist_obj.test(flatten)
+    trn = mnist_obj.train(flatten=flatten, integral=False)
+    tst = mnist_obj.test(flatten=flatten, integral=False)
 
     if shuffle:
         trn = shuffle_data(trn[0], trn[1])
         tst = shuffle_data(tst[0], tst[1])
 
-    # adjust data
-    samples, labels = trn
-    samples = samples if not standardize else adjustment.per_image_standardization(samples)
-    samples = samples if flatten else np.expand_dims(samples, axis=3)
-    labels = label.one_hot_repr(labels)
-    trn = (samples, labels)
+    x_train, y_train = trn
+    x_test, y_test = tst
 
-    samples, labels = tst
-    samples = samples if not standardize else adjustment.per_image_standardization(samples)
-    samples = samples if flatten else np.expand_dims(samples, axis=3)
-    labels = label.one_hot_repr(labels)
-    tst = (samples, labels)
+    x_train, x_test = preprocessing(x_train, x_test, standardize, whitening)
+
+    if one_hot:
+        y_train = label.one_hot_repr(y_train)
+        y_test = label.one_hot_repr(y_test)
+
+    trn = (x_train, y_train)
+    tst = (x_test, y_test)
 
     return trn, tst
 
 
-def load_cifar10(path, flatten=True, standardize=True, shuffle=True, one_hot=True):
-    # load data
+def load_cifar10(path, flatten=False, standardize=True, whitening=True, shuffle=True, one_hot=True):
     cifar_obj = cifar10(path)
-    trn = cifar_obj.train(flatten=flatten, integral=True)
-    tst = cifar_obj.test(flatten=flatten, integral=True)
+    trn = cifar_obj.train(flatten=flatten, integral=False)
+    tst = cifar_obj.test(flatten=flatten, integral=False)
+    print(trn[0].shape, trn[1].shape, trn[0].dtype)
 
     if shuffle:
         trn = shuffle_data(trn[0], trn[1])
         tst = shuffle_data(tst[0], tst[1])
 
-    # adjust data
-    samples, labels = trn
-    samples = samples if not standardize else adjustment.per_image_standardization(samples)
-    labels = label.one_hot_repr(labels) if one_hot else labels
-    trn = (samples, labels)
+    x_train, y_train = trn
+    x_test, y_test = tst
 
-    samples, labels = tst
-    samples = samples if not standardize else adjustment.per_image_standardization(samples)
-    labels = label.one_hot_repr(labels) if one_hot else labels
-    tst = (samples, labels)
+    x_train, x_test = preprocessing(x_train, x_test, standardize, whitening)
+
+    if one_hot:
+        y_train = label.one_hot_repr(y_train)
+        y_test = label.one_hot_repr(y_test)
+
+    trn = (x_train, y_train)
+    tst = (x_test, y_test)
 
     return trn, tst
 
