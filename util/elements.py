9158f481a1e4d0d2f477ece7bd19871c6dd795ee
M. Wöllhaf <woellhaf@tu-berlin.de>
2018-02-22 15:14:37 +0100

    Added batch_norm layer and adjusted weight initializer for ReLU.

--- util/elements.py
+++ util/elements.py
@@ -2,13 +2,32 @@ import tensorflow as tf
 
 
 def weight_variable(shape=None, name='weight_variable'):
-    initial = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
-    return tf.get_variable(name=name, shape=shape, initializer=initial)
+    # init = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
+    init = tf.variance_scaling_initializer(
+        scale=1.0, mode='fan_out', distribution='uniform', seed=None, dtype=tf.float32)
+    return tf.get_variable(name=name, shape=shape, initializer=init)
 
 
 def bias_variable(shape=None, name='bias_variable'):
     initial = tf.constant(0.1, shape=shape) if shape is not None else None
     return tf.get_variable(name=name, initializer=initial)
+
+
+def batch_norm(input_tensor, is_training=False, decay=0.999, variance_epsilon=0.001):
+    dim = input_tensor.get_shape()[-1]
+    mean = tf.get_variable(name='mean_variable', initializer=tf.constant(0.0, shape=[dim]), trainable=False)
+    var = tf.get_variable(name='var_variable', initializer=tf.constant(0.0, shape=[dim]), trainable=False)
+    beta = tf.get_variable(name='beta_variable', initializer=tf.constant(0.0, shape=[dim]))
+    gamma = tf.get_variable(name='gamma_variable', initializer=tf.constant(1.0, shape=[dim]))
+
+    if is_training:
+        batch_mean, batch_var = tf.nn.moments(input_tensor, keep_dims=False)
+        train_mean = tf.assign(mean, mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(var, var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(input_tensor, batch_mean, batch_var, beta, gamma, variance_epsilon)
+    else:
+        return tf.nn.batch_normalization(input_tensor, mean, var, beta, gamma, variance_epsilon)
 
 
 def fc_layer(input_tensor, dim_in, dim_out, act=tf.nn.relu, dropout=None, name='fc_layer'):
@@ -29,7 +48,8 @@ def fc_decode_layer(input_tensor, act=tf.nn.relu, dropout=None, name='fc_decode_
 
 
 def conv2d_layer(input_tensor, fshape, nchannels, nfilters,
-                 strides=[1, 1, 1, 1], act=tf.nn.relu, dropout=None, padding='SAME', name='conv2d_layer'):
+                 strides=[1, 1, 1, 1], act=tf.nn.relu, dropout=None,
+                 padding='SAME', name='conv2d_layer'):
 
     with tf.name_scope(name):
         W = weight_variable([fshape[0], fshape[1], nchannels, nfilters])
@@ -38,78 +58,17 @@ def conv2d_layer(input_tensor, fshape, nchannels, nfilters,
         return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
+def conv2d_layer_bn(input_tensor, fshape, nchannels, nfilters,
+                    strides=[1, 1, 1, 1], act=tf.nn.relu, dropout=None, is_training=False,
+                    padding='SAME', name='conv2d_layer_bn'):
+
+    with tf.name_scope(name):
+        W = weight_variable([fshape[0], fshape[1], nchannels, nfilters])
+        z = tf.nn.conv2d(input_tensor, W, strides=strides, padding=padding)
+        y = act(batch_norm(z, is_training))
+        return tf.nn.dropout(y, dropout) if dropout is not None else y
+
+
 def max_pool_layer(input_tensor, ps=2, ss=2, name='max_pool_layer'):
     with tf.name_scope(name):
         return tf.nn.max_pool(input_tensor, ksize=[1, ps, ps, 1], strides=[1, ss, ss, 1], padding='SAME')
-
-
-def res_block(input_tensor, fshape, nchannels, nfilters,
-              strides=None, act=tf.nn.relu, dropout=None, padding=None, name='res_block'):
-    """ Residual block from 'Deep Residual Learning for Image Recognition'.
-        https://arxiv.org/abs/1512.03385
-
-    Uses dimension matching strategy 'B' (1x1 convolution).
-
-    TODO: Integrate batch norm.
-
-    Args:
-      input_tensor: Input Tensor.
-      fshape: List or tuple with filter dimensions.
-      nchannels: Last dimension of input_tensor.
-      nfilters: Output dimension.
-      strides: Not implemented.
-      act: Activation function (default: tf.nn.relu).
-      dropout: Not implemented.
-      padding: Not implemented.
-      name: Name of residual block.
-
-    Returns:
-      output_tensor
-    """
-    with tf.name_scope(name):
-        x = conv2d_layer(input_tensor, fshape, nchannels, nfilters, act=act, name=name+'_conv1')
-        x = conv2d_layer(x, fshape, nfilters, nfilters, act=tf.identity, name=name+'_conv2')
-
-        if nchannels != nfilters:
-            input_matched = conv2d_layer(input_tensor, [1, 1], nchannels, nfilters,
-                                         act=tf.nn.identity, name=name+'_matching')
-            return act(x + input_matched)
-        else:
-            return act(x + input_tensor)
-
-
-def res_block_bottleneck(input_tensor, fshape, nchannels, nfilters, nfilters_out,
-                         strides=None, act=tf.nn.relu, dropout=None, padding=None, name='res_block'):
-    """ Residual block with bottleneck from 'Deep Residual Learning for Image Recognition'.
-        https://arxiv.org/abs/1512.03385
-
-    Uses dimension matching strategy 'B' (1x1 convolution).
-
-    TODO: Integrate batch norm.
-
-    Args:
-      input_tensor: Input Tensor.
-      fshape: List or tuple with filter dimensions.
-      nchannels: Last dimension of input_tensor.
-      nfilters: Bottleneck dimension.
-      nfilters_out: Output dimension.
-      strides: Not implemented.
-      act: Activation function (default: tf.nn.relu).
-      dropout: Not implemented.
-      padding: Not implemented.
-      name: Name of residual block.
-
-    Returns:
-      output_tensor
-    """
-    with tf.name_scope(name):
-        x = conv2d_layer(input_tensor, [1, 1], nchannels, nfilters, act=act, name=name+'_conv1')
-        x = conv2d_layer(x, fshape, nfilters, nfilters, act=act, name=name+'_conv2')
-        x = conv2d_layer(x, [1, 1], nfilters, nfilters_out, act=tf.identity, name=name+'_conv3')
-
-        if nchannels != nfilters_out:
-            input_matched = conv2d_layer(input_tensor, [1, 1], nchannels, nfilters_out,
-                                         act=tf.nn.identity, name=name+'_matching')
-            return act(x + input_matched)
-        else:
-            return act(x + input_tensor)
