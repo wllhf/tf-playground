f23a832bfedafe9623e537f739a529cabd49098a
M. Wöllhaf <woellhaf@tu-berlin.de>
2018-02-23 14:53:07 +0100

    Fixed batch_norm.

--- util/elements.py
+++ util/elements.py
@@ -14,14 +14,14 @@ def bias_variable(shape=None, name='bias_variable'):
 
 
 def batch_norm(input_tensor, is_training=False, decay=0.999, variance_epsilon=0.001):
-    dim = input_tensor.get_shape()[-1]
-    mean = tf.get_variable(name='mean_variable', initializer=tf.constant(0.0, shape=[dim]), trainable=False)
-    var = tf.get_variable(name='var_variable', initializer=tf.constant(0.0, shape=[dim]), trainable=False)
-    beta = tf.get_variable(name='beta_variable', initializer=tf.constant(0.0, shape=[dim]))
-    gamma = tf.get_variable(name='gamma_variable', initializer=tf.constant(1.0, shape=[dim]))
+    shape = input_tensor.get_shape()
+    mean = tf.get_variable(name='mean_variable', initializer=tf.constant(0.0, shape=[shape[-1]]), trainable=False)
+    var = tf.get_variable(name='var_variable', initializer=tf.constant(0.0, shape=[shape[-1]]), trainable=False)
+    beta = tf.get_variable(name='beta_variable', initializer=tf.constant(0.0, shape=[shape[-1]]))
+    gamma = tf.get_variable(name='gamma_variable', initializer=tf.constant(1.0, shape=[shape[-1]]))
 
     if is_training:
-        batch_mean, batch_var = tf.nn.moments(input_tensor, keep_dims=False)
+        batch_mean, batch_var = tf.nn.moments(input_tensor, [i for i in range(len(shape)-1)], keep_dims=False)
         train_mean = tf.assign(mean, mean * decay + batch_mean * (1 - decay))
         train_var = tf.assign(var, var * decay + batch_var * (1 - decay))
         with tf.control_dependencies([train_mean, train_var]):
@@ -35,6 +35,14 @@ def fc_layer(input_tensor, dim_in, dim_out, act=tf.nn.relu, dropout=None, name='
         W = weight_variable([dim_in, dim_out])
         b = bias_variable([dim_out])
         y = act(tf.matmul(input_tensor, W) + b)
+        return tf.nn.dropout(y, dropout) if dropout is not None else y
+
+
+def fc_layer_bn(input_tensor, dim_in, dim_out, act=tf.nn.relu, dropout=None, training=False, name='fc_layer'):
+    with tf.name_scope(name):
+        W = weight_variable([dim_in, dim_out])
+        z = tf.matmul(input_tensor, W)
+        y = act(batch_norm(z, training))
         return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
@@ -58,15 +66,17 @@ def conv2d_layer(input_tensor, fshape, nchannels, nfilters,
         return tf.nn.dropout(y, dropout) if dropout is not None else y
 
 
-def conv2d_layer_bn(input_tensor, fshape, nchannels, nfilters,
-                    strides=[1, 1, 1, 1], act=tf.nn.relu, dropout=None, is_training=False,
+def conv2d_layer_bn(input_tensor, fshape, nchannels, nfilters, strides=[1, 1, 1, 1],
+                    act=tf.nn.relu, dropout=None, batchnorm=True, training=False,
                     padding='SAME', name='conv2d_layer_bn'):
 
     with tf.name_scope(name):
         W = weight_variable([fshape[0], fshape[1], nchannels, nfilters])
         z = tf.nn.conv2d(input_tensor, W, strides=strides, padding=padding)
-        y = act(batch_norm(z, is_training))
-        return tf.nn.dropout(y, dropout) if dropout is not None else y
+        y = act(batch_norm(z, training)) if batchnorm else act(z+bias_variable([nfilters]))
+        y = tf.nn.dropout(y, dropout) if dropout is not None else y
+
+    return y
 
 
 def max_pool_layer(input_tensor, ps=2, ss=2, name='max_pool_layer'):
